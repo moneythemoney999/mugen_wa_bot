d096c58877b35ecec133f97c9fc6c5f1b0ef104b
moneythemoney999 <193152889+moneythemoney999@users.noreply.github.com>
2026-04-29 23:30:01 -0400

    Traduction(es,fr,ht) de la commande tag et correction español de la commande infos.

--- commandes/xprofil.js
+++ commandes/xprofil.js
@@ -2,6 +2,7 @@
 
 //les imports dont on a besoin
 import { jidNormalizedUser } from '@whiskeysockets/baileys';
+import { traduire } from '../outils/langue.js';
 
 //exporte et logique
 export default {
@@ -9,12 +10,14 @@ export default {
     description: "Piquer la tof profil des gens",
     categorie: "Groupes && Privé",
     infos: `Récupérer la photo profil de plusieurs personnes.
-\`\`\`Utilisation : .xprofil @tag1 569... @tag2\`\`\`
-Envoie un album groupé si plusieurs cibles sont trouvées.`,
+Utilisation : \`.xprofil @tag1 569... @tag2 509...\` ou juste \`.xprofil\` pour la profil du chat actuel.`,
 
     execute: async ({ sock, message, args, nomSession }) => {
         const jid = message.key.remoteJid;
         const estGroupe = jid.endsWith('@g.us');
+
+        //"Raccourci" de traduction importer depui le fichier outils/langue.js
+	const trad = (cle, vars = {}) => traduire(nomSession, 'commandes', 'xprofil', { [cle]: vars })[cle];
 
         let ciblesSujets = new Set();
         const contextInfo = message.message?.extendedTextMessage?.contextInfo;
@@ -44,7 +47,9 @@ Envoie un album groupé si plusieurs cibles sont trouvées.`,
         //2. traitement et recuperation
         for (const cible of listeCibles) {
             const estCibleGroupe = cible.endsWith('@g.us');
-            const nom = estCibleGroupe ? "du groupe" : `@${cible.split('@')[0]}`;
+            const nom = estCibleGroupe
+		? trad('msg.cible_groupe') || "du groupe"
+		: trad('msg.utilisateur', {utilisateur: cible.split('@')[0]}) || `de @${cible.split('@')[0]}`;
 
             try {
                 const url = await sock.profilePictureUrl(cible, 'image');
@@ -58,14 +63,14 @@ Envoie un album groupé si plusieurs cibles sont trouvées.`,
         let legendeFinale = "";
         if (reussis.length > 0) {
             const nomsReussis = reussis.map(r => r.nom).join(', ');
-            legendeFinale += `Voici la photo de profil de ${nomsReussis}`; //legende pour les photos qu'on a reussi a recuperer
+            legendeFinale += trad('msg.legende_reussis', {reussis: nomsReussis}) || `Voici la photo de profil ${nomsReussis}.`; //legende pour les photos qu'on a reussi a recuperer
         }
 
         if (echoues.length > 0) {
             const nomsEchoues = echoues.map(e => e.nom).join(', ');
             if (legendeFinale) legendeFinale += "\n\n";
 	    //puis ceux qui ont echoue
-            legendeFinale += `> Impossible de récupérer la photo de profil de ${nomsEchoues}. Il peut ne pas en avoir ou l'a protégé.`;
+            legendeFinale += trad('msg.legende_non_reussis', {echoues: nomsEchoues}) || `> Impossible de récupérer la photo de profil de ${nomsEchoues}. Il peut ne pas en avoir ou l'a protégé.`;
         }
 
         const toutesMentions = [...reussis.map(r => r.jid), ...listeCibles];
@@ -95,3 +100,4 @@ Envoie un album groupé si plusieurs cibles sont trouvées.`,
         }
     }
 };
+
